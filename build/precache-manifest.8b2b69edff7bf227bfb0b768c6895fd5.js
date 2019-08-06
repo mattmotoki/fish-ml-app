5acff9009df31f4aebaaa2a38771bbfac8c14030
matt <mhmotoki@gmail.com>
2019-08-05 21:24:15 -0700

    Upload sample react app

---
+++ build/precache-manifest.8b2b69edff7bf227bfb0b768c6895fd5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8e302ddfaddb6ed505743ad160a7e8b0",
+    "url": "/retinanetjs-example-app/index.html"
+  },
+  {
+    "revision": "1350e4741e864acfbee6",
+    "url": "/retinanetjs-example-app/static/css/main.5863790c.chunk.css"
+  },
+  {
+    "revision": "d6574ee07c1de98b0945",
+    "url": "/retinanetjs-example-app/static/js/2.7c90468e.chunk.js"
+  },
+  {
+    "revision": "1350e4741e864acfbee6",
+    "url": "/retinanetjs-example-app/static/js/main.87f5796f.chunk.js"
+  },
+  {
+    "revision": "7dcceb14c0ef990927e8",
+    "url": "/retinanetjs-example-app/static/js/runtime~main.1ac5b0a7.js"
+  }
+]);
